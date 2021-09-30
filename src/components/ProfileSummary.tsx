7daf3e7c33f426c32ba69625d3c416d9fcaff07d
Joel Besada <joel.besada@gmail.com>
2021-09-30 10:40:24 +0200

    Add button to initiate floor sum calculation

--- src/components/ProfileSummary.tsx
+++ src/components/ProfileSummary.tsx
@@ -5,7 +5,7 @@ import {
   Flex,
   Spinner,
   HStack,
-  Icon,
+  Button,
   Stat,
   StatLabel,
   StatNumber,
@@ -25,6 +25,7 @@ const ProfileSummary = ({
   ensName?: string
   address?: string
 }) => {
+  const [active, setActive] = useState(false)
   const [progress, setProgress] = useState<{
     total: number
     numLoaded: number
@@ -33,6 +34,7 @@ const ProfileSummary = ({
   const [floorTotal, setFloorTotal] = useState(0)
 
   useEffect(() => {
+    if (!active) return
     fetchAllAssetsForUser({
       userName,
       ensName,
@@ -59,7 +61,7 @@ const ProfileSummary = ({
         }))
       },
     })
-  }, [userName, ensName, address])
+  }, [userName, ensName, address, active])
 
   return (
     <Box px={3} width="100vw" maxWidth="480px">
@@ -69,6 +71,7 @@ const ProfileSummary = ({
         width="100%"
         borderRadius="md"
         border="1px solid"
+        minHeight="90px"
         borderColor={useColorModeValue('gray.200', 'transparent')}
         my="4"
         py="4"
@@ -87,68 +90,79 @@ const ProfileSummary = ({
           right="-16px"
           transform="translateY(-50%)"
         />
-        <Progress
-          bg="transparent"
-          position="absolute"
-          width="100%"
-          size="xs"
-          top={0}
-          m={0}
-          left={0}
-          isIndeterminate={progress.numLoaded === 0}
-          value={(progress.numLoaded / progress.total) * 100}
-          transition="opacity 250ms 100ms ease"
-          opacity={progress.complete ? 0 : 1}
-          sx={{
-            '> div': {
-              transition: 'width 250ms ease',
-            },
-          }}
-        />
-        <HStack spacing="8">
-          <Stat minWidth="150px">
-            <StatLabel whiteSpace="nowrap" opacity="0.75" mb="1px">
-              Total Floor Value
-            </StatLabel>
-            <StatNumber
+        {active ? (
+          <>
+            <Progress
+              bg="transparent"
+              position="absolute"
+              width="100%"
+              size="xs"
+              top={0}
               m={0}
-              whiteSpace="nowrap"
-              display="flex"
-              minHeight="35px"
-              alignItems="center"
-            >
-              <EthereumIcon mt="-3px" />
-              <Text>
-                {progress.numLoaded ? Math.round(floorTotal * 100) / 100 : null}
-              </Text>
-              {!progress.complete ? (
-                <Spinner size="sm" mx="2" mt="-3px" />
-              ) : null}
-            </StatNumber>
-          </Stat>
-          <Stat minWidth="150px">
-            <StatLabel whiteSpace="nowrap" opacity="0.75" mb="1px">
-              Average Item Floor
-            </StatLabel>
-            <StatNumber
-              m={0}
-              whiteSpace="nowrap"
-              display="flex"
-              minHeight="35px"
-              alignItems="center"
-            >
-              <EthereumIcon mt="-3px" />
-              <Text>
-                {progress.numLoaded
-                  ? Math.round((floorTotal / progress.numLoaded) * 100) / 100
-                  : null}
-              </Text>
-              {!progress.complete ? (
-                <Spinner size="sm" mx="2" mt="-3px" />
-              ) : null}
-            </StatNumber>
-          </Stat>
-        </HStack>
+              left={0}
+              isIndeterminate={progress.numLoaded === 0}
+              value={(progress.numLoaded / progress.total) * 100}
+              transition="opacity 250ms 100ms ease"
+              opacity={progress.complete ? 0 : 1}
+              sx={{
+                '> div': {
+                  transition: 'width 250ms ease',
+                },
+              }}
+            />
+            <HStack spacing="8">
+              <Stat minWidth="150px">
+                <StatLabel whiteSpace="nowrap" opacity="0.75" mb="1px">
+                  Total Floor Value
+                </StatLabel>
+                <StatNumber
+                  m={0}
+                  whiteSpace="nowrap"
+                  display="flex"
+                  minHeight="35px"
+                  alignItems="center"
+                >
+                  <EthereumIcon mt="-3px" />
+                  <Text>
+                    {progress.numLoaded
+                      ? Math.round(floorTotal * 100) / 100
+                      : null}
+                  </Text>
+                  {!progress.complete ? (
+                    <Spinner size="sm" mx="2" mt="-3px" />
+                  ) : null}
+                </StatNumber>
+              </Stat>
+              <Stat minWidth="150px">
+                <StatLabel whiteSpace="nowrap" opacity="0.75" mb="1px">
+                  Average Item Floor
+                </StatLabel>
+                <StatNumber
+                  m={0}
+                  whiteSpace="nowrap"
+                  display="flex"
+                  minHeight="35px"
+                  alignItems="center"
+                >
+                  <EthereumIcon mt="-3px" />
+                  <Text>
+                    {progress.numLoaded
+                      ? Math.round((floorTotal / progress.numLoaded) * 100) /
+                        100
+                      : null}
+                  </Text>
+                  {!progress.complete ? (
+                    <Spinner size="sm" mx="2" mt="-3px" />
+                  ) : null}
+                </StatNumber>
+              </Stat>
+            </HStack>
+          </>
+        ) : (
+          <Button onClick={() => setActive(true)} variant="outline">
+            Calculate Collection Value
+          </Button>
+        )}
       </Flex>
     </Box>
   )
